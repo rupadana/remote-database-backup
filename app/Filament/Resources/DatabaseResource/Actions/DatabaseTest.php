9985c85cdc77b4b6301fce2b11f9477d262c6ef6
Rupadana <34137674+rupadana@users.noreply.github.com>
2024-07-27 10:06:58 +0800

    Update DatabaseTest.php

--- app/Filament/Resources/DatabaseResource/Actions/DatabaseTest.php
+++ app/Filament/Resources/DatabaseResource/Actions/DatabaseTest.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\DatabaseResource\Actions;
 use App\Models\Database;
 use Filament\Actions\Action;
 use Exception;
+use Filament\Notifications\Notification;
 use mysqli;
 
 class DatabaseTest extends Action
@@ -15,30 +16,37 @@ class DatabaseTest extends Action
 
         $this->action(function (Database $record) {
 
+            $record = $record->data[0]['data'];
 
-            $host = $record->data[0]['data']['host'];
-            $username = $record->data[0]['data']['username'];
-            $password = $record->data[0]['data']['password'];
-            $database = $record->data[0]['data']['database'];
-            $port = $record->data[0]['data']['port'];
+
+            $host = $record['host'];
+            $username = $record['username'];
+            $password = $record['password'];
+            $database = $record['database'];
+            $port = $record['port'];
 
             $mysqli = new \mysqli($host, $username, $password, $database);
 
             // Check if the query was successful
             if ($mysqli->connect_error) {
-                dd('Error executing query: ' . $mysqli->error);
+                $this->failureNotification(
+                    Notification::make()
+                        ->title("Connection Failed")
+                        ->body($mysqli->error)
+                );
+                $this->sendFailureNotification();
             }else{
                 $this->sendSuccessNotification();
                 $mysqli->close();
             }
-
-
         });
 
-        $this->label('Connection Test');
+        $this->label('Test Connection');
         $this->color('info');
 
         // Here we use the record's ID
         $this->successNotificationTitle('Connection OK ');
+
+        
     }
 }
